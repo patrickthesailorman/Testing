1ebba45c6d05ba40d74d9ccbb7bb877832a419c0
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-03 17:14:54 -0400

    Add Buzz test

--- FizzBuzz.Library.Tests/FIzzBuzzerTests.cs
+++ FizzBuzz.Library.Tests/FIzzBuzzerTests.cs
@@ -11,19 +11,6 @@ namespace FizzBuzz.Library.Tests
     public class FizzBuzzerTests
     {
         [Test]
-        public void Buzzer_When1_returns1();
-        {
-            // Arrange
-            int input = 1;
-
-            // Act
-            FizzBuzzer.GetValue(input);
-
-            // Assert
-            Assert.AreEqual("1", output);
-        }
-
-        [Test]
         public void Buzzer_WhenDefault_ReturnsInput([Values(1, 2, 4)] int input)
         {
             string output = FizzBuzzer.GetValue(input);
@@ -33,9 +20,22 @@ namespace FizzBuzz.Library.Tests
         [Test]
         public void Buzzer_When3_ReturnsFizz()
         {
+            // Arrange
             int input = 3;
+            // Act
             string output = FizzBuzzer.GetValue(input);
+            // Assert
             Assert.AreEqual("Fizz", output);
+        }
+        [Test]
+        public void Buzzer_When5_ReturnsBuzz()
+        {
+            // Arrange
+            int input = 5;
+            // Act
+            string output = FizzBuzzer.GetValue(input);
+            // Assert
+            Assert.AreEqual("Buzz", output);
         }
     }
 }
