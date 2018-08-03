675863a923316b4f297674ec17433b940eed6550
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-02 21:27:21 -0400

    Add SampleTest

--- FizzBuzz.Library.Tests/FIzzBuzzerTests.cs
+++ FizzBuzz.Library.Tests/FIzzBuzzerTests.cs
@@ -6,5 +6,10 @@ namespace FizzBuzz.Library.Tests
     [Test Fixture]
     public class FizzBuzzerTests
     {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.pass();
+        }
     }
 }
