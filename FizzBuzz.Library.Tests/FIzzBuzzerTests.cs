f7bd22c48355116acd9dda5dd66b01d2c8b76c2c
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-03 16:58:27 -0400

    Add Fizz Test

--- FizzBuzz.Library.Tests/FIzzBuzzerTests.cs
+++ FizzBuzz.Library.Tests/FIzzBuzzerTests.cs
@@ -22,12 +22,21 @@ namespace FizzBuzz.Library.Tests
             // Assert
             Assert.AreEqual("1", output);
         }
+
         [Test]
         public void Buzzer_When2_Returns2()
         {
             int input = 2;
             string output = FizzBuzzer.GetValue(input);
             Assert.AreEqual("2", output);
+        }
+
+        [Test]
+        public void Buzzer_When3_ReturnsFizz()
+        {
+            int input = 3;
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Fizz", output);
         }
     }
 }
