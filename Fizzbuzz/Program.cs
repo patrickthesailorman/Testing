344fe7dcf8767a4b2d740b3a0f08c14d8b3128b2
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-02 21:22:34 -0400

    Add FizzBuzz for loop

--- Fizzbuzz/Program.cs
+++ Fizzbuzz/Program.cs
@@ -6,7 +6,11 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for ( int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
         }
     }
 }
