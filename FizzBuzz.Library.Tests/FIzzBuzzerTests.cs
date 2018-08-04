82be6e27e23d9e372804f62780e5710342becd60
patrickthesailorman <patrickthesailorman@gmail.com>
2018-08-04 17:08:23 -0400

    Add values 18, 19, 20

--- FizzBuzz.Library.Tests/FIzzBuzzerTests.cs
+++ FizzBuzz.Library.Tests/FIzzBuzzerTests.cs
@@ -11,7 +11,7 @@ namespace FizzBuzz.Library.Tests
     public class FizzBuzzerTests
     {
         [Test]
-        public void Buzzer_WhenDefault_ReturnsInput([Values(1, 2, 4, 7, 8, 11, 13, 14, 16, 17)] int input)
+        public void Buzzer_WhenDefault_ReturnsInput([Values(1, 2, 4, 7, 8, 11, 13, 14, 16, 17, 19)] int input)
         {
             // Arrange
             
@@ -22,7 +22,7 @@ namespace FizzBuzz.Library.Tests
         }
 
         [Test]
-        public void Buzzer_WhenDiv3_ReturnsFizz([Values(3, 6, 9, 12)] int input)
+        public void Buzzer_WhenDiv3_ReturnsFizz([Values(3, 6, 9, 12, 18)] int input)
         {
             // Arrange
             // int input = 3;
@@ -33,7 +33,7 @@ namespace FizzBuzz.Library.Tests
         }
 
         [Test]
-        public void Buzzer_When5_ReturnsBuzz([Values(5, 10)] int input)
+        public void Buzzer_When5_ReturnsBuzz([Values(5, 10, 20)] int input)
         {
             // Arrange
             // int input = 5;
